fcc6dc28f6fd32246d24302f4b6328adb49eef73
Panchorn Lertvipada <nonpcn@gmail.com>
2019-07-03 00:25:00 +0700

    Implement fibo method without recursion

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,14 @@
 public class Fibonacci {
     public static void main(String[] args) {
         Long start = System.currentTimeMillis();
-        Long result = fibonacciWithRecursion(30L);
+        Long result = fibonacciWithRecursion(50L);
         System.out.println("result of fibonacciWithRecursion is " + result);
         System.out.println("using " + (System.currentTimeMillis() - start) + " ms");
         
-        fibonacciWithoutRecursion(10L);
+        start = System.currentTimeMillis();
+        result = fibonacciWithoutRecursion(50L);
+        System.out.println("result of fibonacciWithoutRecursion is " + result);
+        System.out.println("using " + (System.currentTimeMillis() - start) + " ms");
     }
 
     public static Long fibonacciWithRecursion(Long input) {
@@ -17,8 +20,21 @@ public class Fibonacci {
         return r1 + r2;
     }
 
-    public static void fibonacciWithoutRecursion(Long input) {
-        System.out.println("input is " + input);
+    public static Long fibonacciWithoutRecursion(Long input) {
+        Long current = 0L;
+        Long p1 = 0L;
+        Long p2 = 0L;
+        Long sum = 0L;
+        while (current < input) {
+            if (current == 0L || current == 1L) {
+                p1 = current;
+            }
+            sum = p1 + p2;
+            p2 = p1;
+            p1 = sum;
+            current++;  
+        }
+        return sum;
     }
 
 }
