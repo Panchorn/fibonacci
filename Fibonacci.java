effa3c970f95eb704270216aa08942446911dc55
Panchorn Lertvipada <nonpcn@gmail.com>
2019-07-03 00:33:37 +0700

    Start loop with 2

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,10 +5,10 @@ public class Fibonacci {
         System.out.println("result of fibonacciWithRecursion is " + result);
         System.out.println("using " + (System.nanoTime() - start) + " nano");
         
-        start = System.nanoTime();
-        result = fibonacciWithoutRecursion(50L);
-        System.out.println("result of fibonacciWithoutRecursion is " + result);
-        System.out.println("using " + (System.nanoTime() - start) + " nano");
+        Long start2 = System.nanoTime();
+        Long result2 = fibonacciWithoutRecursion(50L);
+        System.out.println("result of fibonacciWithoutRecursion is " + result2);
+        System.out.println("using " + (System.nanoTime() - start2) + " nano");
     }
 
     public static Long fibonacciWithRecursion(Long input) {
@@ -21,14 +21,11 @@ public class Fibonacci {
     }
 
     public static Long fibonacciWithoutRecursion(Long input) {
-        Long current = 0L;
-        Long p1 = 0L;
+        Long current = 2L;
+        Long p1 = 1L;
         Long p2 = 0L;
         Long sum = 0L;
         while (current < input) {
-            if (current == 0L || current == 1L) {
-                p1 = current;
-            }
             sum = p1 + p2;
             p2 = p1;
             p1 = sum;
