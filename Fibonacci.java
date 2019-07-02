f080bc8f543f26a15497a27d3d8bc529589dc473
Panchorn Lertvipada <nonpcn@gmail.com>
2019-07-02 23:24:53 +0700

    Initial class and method

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,9 +1,14 @@
 public class Fibonacci {
     public static void main(String[] args) {
         fibonacciWithRecursion(10L);
+        fibonacciWithoutRecursion(10L);
     }
 
     public static void fibonacciWithRecursion(Long input) {
+        System.out.println("input is " + input);
+    }
+
+    public static void fibonacciWithoutRecursion(Long input) {
         System.out.println("input is " + input);
     }
 
